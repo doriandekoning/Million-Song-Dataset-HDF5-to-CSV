6428d41848c3eba2d162ef382b4f37260e9b7938
doriandekoning <doriandekoning@gmail.com>
2018-05-23 17:09:53 +0200

    Fix attributes

--- msdHDF5toCSV.py
+++ msdHDF5toCSV.py
@@ -125,12 +125,13 @@ def main():
     	os.makedirs("out")
     outputFile1 = open('out/SongCSV0.csv', 'w')
 
-    csvRowString = ("SongID,albumName,albumID,artistID,artistLatitude,artistLocation,artistLongitude,artistFamiliarity,artistHotttnesss,artistmbid,artistPlaymeid,artist7digitalid,artistTerms,artistTermsFreq,artistTermsWeight,artistMBTags,artistMBTagsCount,analysisSampleRate,audioMD5,endOfFadeIn,startOfFadeOut,energy,release,release7digitalid,songHotness,track7digitalid,similarartists,loudness,mode,modeConfidence,artistName,danceability,duration,keySignature,keySignatureConfidence,tempo,timeSignature,timeSignatureConfidence,title,year,trackID,segmentsStart,segmentsConfidence,segmentsPitches,segmentsTimbre,segmentsLoudnessMax,segmentsLoudnessMaxTime,segmentsLoudnessStart,sectionStarts,sectionsConfidence,beatsStart,beatsConfidence,barsStart,barsConfidence,tatumsStart,tatumsConfidence")
+    csvHeaderString = ("SongID,albumName,albumID,artistID,artistLatitude,artistLocation,artistLongitude,artistFamiliarity,artistHotttnesss,artistmbid,artistPlaymeid,artist7digitalid,artistTerms,artistTermsFreq,artistTermsWeight,artistMBTags,artistMBTagsCount,analysisSampleRate,audioMD5,endOfFadeIn,startOfFadeOut,energy,release,release7digitalid,songHotness,track7digitalid,similarartists,loudness,mode,modeConfidence,artistName,danceability,duration,keySignature,keySignatureConfidence,tempo,timeSignature,timeSignatureConfidence,title,year,trackID,segmentsStart,segmentsConfidence,segmentsPitches,segmentsTimbre,segmentsLoudnessMax,segmentsLoudnessMaxTime,segmentsLoudnessStart,sectionStarts,sectionsConfidence,beatsStart,beatsConfidence,barsStart,barsConfidence,tatumsStart,tatumsConfidence")
 
-    csvAttributeList = re.split('\W+', csvRowString)
+    csvAttributeList = re.split('\W+', csvHeaderString)
     for i, v in enumerate(csvAttributeList):
         csvAttributeList[i] = csvAttributeList[i].lower()
-    writeheader(outputFile1, csvRowString)
+    writeheader(outputFile1, csvHeaderString)
+    csvRowString = ""
     print("started converting")
 
 
@@ -223,7 +224,7 @@ def main():
             #print song count
             csvRowString += str(song.songCount) + ","
 
-            for attribute in  re.split('\W+', csvRowString):
+            for attribute in  re.split('\W+', csvHeaderString):
                 # print "Here is the attribute: " + attribute + " \n"
                 if hasattr(song, attribute) :
                     csvRowString += getattr(song, attribute)

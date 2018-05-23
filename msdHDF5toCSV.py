628916f7d7aa023665b74cf4481b0b7e9e208181
doriandekoning <doriandekoning@gmail.com>
2018-05-23 18:37:22 +0200

    Fix artist terms error

--- msdHDF5toCSV.py
+++ msdHDF5toCSV.py
@@ -177,7 +177,7 @@ def main():
             song.artistPlaymeid = str(hdf5_getters.get_artist_playmeid(songH5File))
             song.artist7digitalid = str(hdf5_getters.get_artist_7digitalid(songH5File))
             #song.artistTerms = str(hdf5_getters.get_artist_terms(songH5File))
-            song.artistTermsFreq = str(hdf5_getters.get_artist_terms_freq(songH5File))
+            # song.artistTermsFreq = str(hdf5_getters.get_artist_terms_freq(songH5File))
             song.artistTermsWeight = str(hdf5_getters.get_artist_terms_weight(songH5File))
             song.artistMBTags = str(hdf5_getters.get_artist_mbtags(songH5File))
             song.artistMBTagsCount = str(hdf5_getters.get_artist_mbtags_count(songH5File))
